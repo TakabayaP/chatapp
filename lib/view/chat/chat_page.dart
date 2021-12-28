89e4fffe4132c39bd6842adeac66b7857cef0c6d
TakabayaP <takabayap@gmail.com>
2021-12-28 10:20:37 +0900

    update: remove unused import

--- lib/view/chat/chat_page.dart
+++ lib/view/chat/chat_page.dart
@@ -2,7 +2,6 @@ import 'package:chatapp/view/chat/chat_list.dart';
 import 'package:chatapp/viewmodel/chat_view_model.dart';
 import 'package:chatapp/widget/chat_text_field.dart';
 import 'package:flutter/material.dart';
-import 'package:chatapp/model/communication.dart' as comm;
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -17,7 +16,7 @@ class ChatPage extends HookConsumerWidget {
         title: const Text("Flutter + Go Chatapp"),
       ),
       body: Stack(
-        children: <Widget>[ChatList(), ChatTextField()],
+        children: <Widget>[const ChatList(), ChatTextField()],
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
