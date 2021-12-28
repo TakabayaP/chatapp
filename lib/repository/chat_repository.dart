df31765f5a46be122bafdf1997fd2b24cba4be2d
TakabayaP <takabayap@gmail.com>
2021-12-28 11:57:09 +0900

    Revert "Feature/20211227/use riverpod"

--- lib/repository/chat_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:chatapp/model/chat_model.dart';
-
-abstract class ChatRepository {
-  Future<void> postChats(String body);
-  Future<List<Chat>> getChats();
-}
--- lib/view/chat/chat_list.dart
+++ lib/view/chat/chat_list.dart
@@ -1,24 +1,33 @@
+import 'dart:async';
+import 'package:chatapp/widget/chat_bubble.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import 'package:chatapp/viewmodel/chat_view_model.dart';
-import 'package:chatapp/view/chat/chat_bubble.dart';
-
-class ChatList extends HookConsumerWidget {
-  const ChatList({Key? key}) : super(key: key);
+class ChatList extends StatelessWidget {
+  const ChatList({Key? key, required this.stream}) : super(key: key);
+  final Stream<List> stream;
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final chat = ref.watch(chatViewModelProvider);
-
+  Widget build(BuildContext context) {
     return SingleChildScrollView(
-        child: ListView.builder(
-            itemCount: chat.chats.length,
-            shrinkWrap: true,
-            reverse: true,
-            padding: const EdgeInsets.only(top: 10, bottom: 55),
-            itemBuilder: (BuildContext context, index) {
-              return ChatBubble(
-                  body: chat.chats[index].body, id: chat.chats[index].userId);
-            }));
+      child: StreamBuilder<List>(
+        builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                shrinkWrap: true,
+                padding: const EdgeInsets.only(top: 10, bottom: 55),
+                itemBuilder: (BuildContext context, index) {
+                  return ChatBubble(
+                      body: snapshot.data![index]['content'],
+                      id: snapshot.data![index]['user_id']);
+                });
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+        stream: stream,
+      ),
+    );
   }
 }
