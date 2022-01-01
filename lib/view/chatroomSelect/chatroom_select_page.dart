0ab5802b212e66afc56bf0f40e1749bfc48c895e
takabayap <takabayap@gmail.com>
2022-01-01 13:08:33 +0900

    update: add ChatroomList

--- lib/view/chatroomSelect/chatroom_select_page.dart
+++ lib/view/chatroomSelect/chatroom_select_page.dart
@@ -2,44 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:chatapp/viewmodel/chatroom_select_view_model.dart';
+import 'package:chatapp/view/chatroomSelect/chatroom_list.dart';
 
 class ChatroomSelect extends HookConsumerWidget {
   const ChatroomSelect({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final chatroomSelect = ref.watch(chatroomSelectViewModelProvider);
+    final viewmodel = ref.watch(chatroomSelectViewModelProvider);
     final TextEditingController _inputUserId = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter + Go Chatapp"),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            TextField(
-              autofocus: true,
-              controller: _inputUserId,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                hintText: 'Input user id...',
-              ),
+      body: Column(
+        children: [
+          const ChatroomList(),
+          TextField(
+            autofocus: true,
+            controller: _inputUserId,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              hintText: 'Input user id...',
             ),
-            ElevatedButton(
-              child: const Text('Enter Chatroom'),
-              onPressed: () {
-                try {
-                  int.parse(_inputUserId.text);
-                } on FormatException {
-                  print("parse error");
-                  return;
-                }
-                chatroomSelect.enterChatroom(
-                    userId: int.parse(_inputUserId.text));
-              },
-            ),
-          ],
-        ),
+            onChanged: (text) {
+              try {
+                int.parse(text);
+              } on FormatException {
+                print("parse error");
+                return;
+              }
+              viewmodel.userId = int.parse(text);
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Enter Chatroom'),
+            onPressed: () {
+              viewmodel.enterChatroom();
+            },
+          ),
+        ],
       ),
     );
   }
