053f4e3588531f27284b3753afc9484ac55c1b34
takabayap <takabayap@gmail.com>
2022-01-02 19:06:43 +0900

    add: chatroom_list

---
+++ lib/view/chatroomSelect/chatroom_list.dart
@@ -0,0 +1,23 @@
+import 'package:chatapp/viewmodel/chatroom_select_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class ChatroomList extends HookConsumerWidget {
+  const ChatroomList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final viewmodel = ref.watch(chatroomSelectViewModelProvider);
+
+    return ListView.builder(
+        itemCount: viewmodel.chatrooms.length, //viewmodel.chatrooms.length,
+        shrinkWrap: true,
+        itemBuilder: (BuildContext context, index) {
+          return Card(
+              child: ListTile(
+            title: Text(viewmodel.chatrooms[index].name),
+            onTap: () => viewmodel.enterChatroom(),
+          ));
+        });
+  }
+}
