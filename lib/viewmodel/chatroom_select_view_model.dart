88154971d8bf5b890695a8a9a9c3fd18235c4b65
takabayap <takabayap@gmail.com>
2022-01-02 20:28:02 +0900

    update: add repository to viewmodel

--- lib/viewmodel/chatroom_select_view_model.dart
+++ lib/viewmodel/chatroom_select_view_model.dart
@@ -2,12 +2,20 @@ import 'package:flutter/foundation.dart';
 import 'package:get/route_manager.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final chatroomSelectViewModelProvider =
-    ChangeNotifierProvider((ref) => ChatroomSelectViewModel());
+import 'package:chatapp/repository/chatroom_repository.dart';
+import 'package:chatapp/repository/mock_chatroom_repository.dart';
+
+final chatroomSelectViewModelProvider = ChangeNotifierProvider(
+    (ref) => ChatroomSelectViewModel(repository: ChatroomRepositoryImpl()));
 
 class ChatroomSelectViewModel extends ChangeNotifier {
-  ChatroomSelectViewModel();
+  ChatroomSelectViewModel({required this.repository}) {
+    _chatrooms = repository.chatrooms;
+  }
+  final ChatroomRepository repository;
   late int userId;
+  List _chatrooms = [];
+  get chatrooms => _chatrooms;
   void enterChatroom() {
     Get.toNamed('/chat_page', arguments: {'userId': userId});
   }
