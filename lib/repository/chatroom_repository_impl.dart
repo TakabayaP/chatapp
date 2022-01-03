e14f25dba1caa3daa3c048e21f40776ac2a3e6ac
TakabayaP <takabayap@gmail.com>
2022-01-03 11:56:45 +0900

    update: get data from datasource

--- lib/repository/chatroom_repository_impl.dart
+++ lib/repository/chatroom_repository_impl.dart
@@ -2,15 +2,18 @@ import 'package:flutter/foundation.dart';
 
 import 'package:chatapp/model/chatroom_model.dart';
 import 'package:chatapp/repository/chatroom_repository.dart';
+import 'package:chatapp/repository/datasource/chatroom/chatroom_data_source.dart';
 
 class ChatroomRepositoryImpl extends ChangeNotifier
     implements ChatroomRepository {
+  ChatroomRepositoryImpl({required this.dataSource});
+  final ChatroomDataSource dataSource;
   @override
-  late List<Chatroom> chatrooms;
+  List<Chatroom> chatrooms = [];
 
   @override
-  Future<void> getChatrooms() {
-    // TODO: implement getChatrooms
-    throw UnimplementedError();
+  void getChatrooms() async {
+    chatrooms = await dataSource.getChatrooms();
+    notifyListeners();
   }
 }
